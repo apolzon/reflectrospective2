519aca59e2e4c496703fd13565b13ad287d2d1b5
Ben Lindsey <ben@carbonfive.com>
2011-08-27 09:55:57 -0700

    added mongodb persistence library, refactored socket events and rewrote client js

--- public/js/board.js
+++ public/js/board.js
@@ -1,70 +1,95 @@
-function adjustTextarea(textarea) {
-  $(textarea).css('height','auto');
-  if ($(textarea).innerHeight() < textarea.scrollHeight) 
-    $(textarea).css('height',textarea.scrollHeight);
+function adjustTextarea( $textarea ) {
+  var scrollHeight = $textarea[0].scrollHeight;
+  $textarea.css( 'height', 'auto' );
+  if( $textarea.innerHeight() < scrollHeight ) {
+    $textarea.css( 'height', scrollHeight );
+  }
 }
 
 $(function() {
-  var socket = io.connect('http://' + document.location.host);
-  socket.on( 'move', function( coords ) {
-    $('#'+coords.id).css('left', coords.x );
-    $('#'+coords.id).css('top', coords.y );
+  var $board = $( '.board' );
+
+  var socket = io.connect();
+
+  socket.on( 'connect', function() {
+    socket.emit( 'enter', $board.attr( 'id' ) );
   });
+
   socket.on( 'add', createCard );
-  socket.on( 'text', function( data ) {
-    $('#'+data.id+' textarea').val(data.text);
-    adjustTextarea($('#'+data.id+' textarea')[0]);
+
+  socket.on( 'move', function( note ) {
+    $( '#' + note.id ).css( 'left', note.x ).css( 'top', note.y );
+  });
+
+  socket.on( 'type', function( note ) {
+    var $textarea = $( '#' + note.id + ' textarea' );
+    $textarea.val( note.text );
+    adjustTextarea( $textarea );
   } );
 
   function createCard( data ) {
     if ( !data ) {
       data = {
-        id: parseInt(Math.random() * 1000000000),
-        x: parseInt(Math.random() * ($('.board').innerWidth() - 296)),
-        y: parseInt(Math.random() * ($('.board').innerHeight() - 300))
+        board : $board.attr( 'id' ),
+        id : Math.floor( Math.random() * 1000000000 ).toString(),
+        x : parseInt( Math.random() * ($board.innerWidth() - 296)),
+        y : parseInt( Math.random() * ($board.innerHeight() - 300)),
+        text : ''
       }
-      socket.emit('add', data);
+      socket.emit( 'add', data );
     }
 
-    var $card = $('<div class="card"><textarea style="height: auto; "></textarea></div>')
-      .attr('id', data.id)
-      .css('left', data.x)
-      .css('top', data.y)
-    $('.board').append($card);
-    $('textarea', $card).focus();
+    var $card = $('<div id="' + data.id + '" class="card"><textarea style="height: auto;">' + data.text + '</textarea></div>')
+      .css( 'left', data.x ).css( 'top', data.y );
+    $board.append( $card );
+    $( 'textarea', $card ).focus();
   }
 
   var dragged;
-  $('.card').live('mousedown', function(e) {
-    var deltaX = e.clientX-this.offsetLeft, deltaY = e.clientY-this.offsetTop;
+  $( '.card' ).live( 'mousedown', function( event ) {
+    var deltaX = event.clientX - this.offsetLeft, deltaY = event.clientY - this.offsetTop;
     dragged = this.id;
+    if( !dragged ) return;
 
-    function move(e) {
-      $('#'+dragged).css('top', e.clientY - deltaY);
-      $('#'+dragged).css('left', e.clientX - deltaX);
-      socket.emit('move', {id:dragged, x:e.clientX - deltaX, y:e.clientY - deltaY});
+    function move( event ) {
+      $( '#' + dragged ).css( 'top', event.clientY - deltaY ).css( 'left', event.clientX - deltaX );
+      socket.emit( 'move', { board: $board.attr( 'id' ), id : dragged, x : event.clientX - deltaX, y : event.clientY - deltaY } );
     }
 
-    $('body').mousemove(move);
+    $( 'body' ).mousemove( move );
 
-    $('body').mouseup(function() {
-      $('body').unbind('mousemove', move);
+    $( 'body' ).mouseup( function( event ) {
+      $( 'body' ).unbind( 'mousemove', move );
+      socket.emit( 'drop', { board : $board.attr( 'id' ), id : dragged, x : event.clientX - deltaX, y : event.clientY - deltaY } );
       dragged = null;
     });
   });
 
-  $('.card textarea').live('keyup', function() {
-    var card = $(this).closest('.card')[0];
-    socket.emit('text', { id:card.id, text:$(this).val() });
-    adjustTextarea(this);
-    return false;
+  var updater;
+  $( '.card textarea' ).live( 'focus', function() {
+    var $this = $( this );
+    $this.last = $this.val();
+    try { clearInterval( updater ) } catch ( e ) {};
+    updater = setInterval( function() {
+      var val = $this.val();
+      if( val !== $this.last ) {
+        socket.emit( 'type', { board : $board.attr( 'id' ), id : $this.parent().attr( 'id' ), text : val } );
+        adjustTextarea( $this );
+        $this.last = val;
+      }
+    }, 100 );
   });
 
-  $('button.create').click(function() {
+  $( '.card textarea' ).live( 'blur', function() {
+    var $this = $( this );
+    socket.emit( 'text', { board : $board.attr( 'id' ), id : $this.parent().attr( 'id' ), text : $this.val() } );
+    if( updater ) clearInterval( updater );
+  });
+
+  $( 'button.create' ).click( function() {
     createCard();
   });
 
   // DEBUG
   window.createCard = createCard;
-
 });
--- server.js
+++ server.js
@@ -1,13 +1,15 @@
-var express = require( 'express' ),
+var nko = require( 'nko' )( 'XeXQ4S+ArH4oslLH' ),
+    express = require( 'express' ),
     sockets = require( 'socket.io' ),
-    nko = require('nko')('XeXQ4S+ArH4oslLH');
+    mongo = require( './lib/mongo.js' ),
+    app = express.createServer(),
+    io = sockets.listen( app );
 
-var app = express.createServer();
-var io = sockets.listen(app);
-io.set('log level', 1); 
+io.set('log level', 3); 
 
 process.on( "uncaughtException", function( error ) {
   console.error( "Uncaught exception: " + error.message );
+  console.trace();
 });
 
 app.configure( function() {
@@ -23,15 +25,75 @@ app.configure( function() {
   });
 });
 
-app.get( "/board/:board", function(request, response) { response.render("board"); } );
+app.get( "/boards/:board", function( request, response ) {
+  mongo.find( 'boards', { name : request.params.board }, function( error, board ) {
+    if( error ) {
+      response.render( "500", { status : 500, error : error } );
+    } else if( board ) {
+      response.render( "boards/view", { board : board } );
+    } else {
+      response.render( "404", { status : 404 } );
+    }
+  });
+});
 
-app.listen( parseInt(process.env.PORT) || 7777 ); 
+app.post( "/boards/:board", function( request, response ) {
+  mongo.save( 'boards', { name : request.params.board }, function( error, board ) {
+    if( error ) {
+      response.render( "500", { status : 500, error : error } );
+    } else {
+      response.render( "boards/view", { board : board } );
+    }
+  });
+});
 
 function rebroadcast( socket, events ) {
-  events.forEach(function(event) {
-    socket.on(event, function(data) { socket.broadcast.emit( event, data ); });
+  events.forEach( function( event ) {
+    socket.on( event, function( data ) {
+      socket.broadcast.emit( event, data );
+    });
   });
 }
-io.sockets.on('connection', function( socket ) {
-  rebroadcast(socket, ['move', 'text', 'add']);
+
+io.sockets.on( 'connection', function( socket ) {
+
+  rebroadcast( socket, [ 'move', 'type' ] );
+
+  socket.on( 'enter', function( board ) {
+    mongo.all( 'notes', { board : board }, function( error, note ) {
+      if( error ) return socket.emit( 'error', error );
+      if( note ) socket.emit( 'add', note );
+    });
+  });
+
+  socket.on( 'add', function( note ) {
+    mongo.save( 'notes', note, function( error, board ) {
+      socket.broadcast.emit( 'add', note );
+    });
+  });
+
+  socket.on( 'drop', function( note ) {
+    mongo.find( 'notes', { id : note.id }, function( error, item ) {
+      if( item ) {
+        item.x = note.x;
+        item.y = note.y;
+        mongo.save( 'notes', item, function( error ) {
+          if( error ) return socket.emit( 'error', error );
+        });
+      }
+    });
+  });
+
+  socket.on( 'text', function( note ) {
+    mongo.find( 'notes', { id : note.id }, function( error, item ) {
+      if( item ) {
+        item.text = note.text;
+        mongo.save( 'notes', item, function( error ) {
+          if( error ) return socket.emit( 'error', error );
+        });
+      }
+    });
+  });
 });
+
+app.listen( parseInt(process.env.PORT) || 7777 ); 
